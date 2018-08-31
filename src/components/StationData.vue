1a22e2f19f05a6101459e238e2d19d45902704f8
Anologicon <pauloraimundi2009@unifebe.edu.br>
2018-08-30 23:26:27 -0300

    prepare data to chart

--- src/components/StationData.vue
+++ src/components/StationData.vue
@@ -2,11 +2,26 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <blockquote>
-          {{ data.lanternId }}
-
-          {{ temperaturs   }}
+        <blockquote v-if="data && !ajax">
+         <bars /> 
         </blockquote>
+        <blockquote v-show="!data">
+          <v-alert
+            :value="true"
+            color="info"
+            icon="info"
+            outline
+          >
+            Selecione uma estação no menu lateral.
+          </v-alert>
+        </blockquote>
+        <v-progress-circular
+            :size="100"
+            :width="10"
+            color="blue"
+            indeterminate=""
+            v-if="ajax"
+          ></v-progress-circular>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -15,6 +30,7 @@
 <script>
 import bus from './../bus.js';
 import axios from 'axios';
+import Bars from './charts/Bars'
 
 export default {
   name: 'StationData',
@@ -22,17 +38,25 @@ export default {
     return {
       data: '',
       temperaturs: '',
+      ajax: false,
     }
   },
   created() {
     this.callStationEvent();
   },
   methods: {
-    getData() {
+    getData(station) {
+      this.data = station;
+
       let URL = 'http://sisei-p.unifebe.edu.br/smight/api/listarTemperaturaMedia.php/' + this.data.lanternId;
 
+      this.ajax = true;
       axios.get(URL).then((response) => {
         this.temperaturs = response.data.temperatura;
+        
+        this.ajax = false;
+
+        this.ordernateDate();
       }).catch((error) => {
         console.error(error);
       });
@@ -40,11 +64,21 @@ export default {
     callStationEvent() {
       bus.$on('callStationData', ($station) => {
         this.getData($station);
-
-        this.data = $station;
       });   
+    },
+    ordernateDate() {
+      var dia = [];
+
+      this.temperaturs.forEach(temperatur => {
+        dia[temperatur.dia] += parseInt(temperatur.valorConvertido, 10)
+      });
+
+      console.log(dia);
     }
   },
+  components: {
+    Bars
+  }
 }
 </script>
 
